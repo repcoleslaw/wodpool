afe128a8ef8df68241188367f8bc99c81ae43523
arr_cole <andrew.rr.cole@gmail.com>
2021-02-18 10:10:33 -0500

    Signup and Login
    Landing Page and Login and Signup Components using templates from material ui

--- wodpool-client/src/Pages/landingPage.js
+++ wodpool-client/src/Pages/landingPage.js
@@ -3,10 +3,15 @@ import React from 'react'
 // Landing page will be the first screen seen handling authentication/
 // single page for both login- and if not already, sign up.
 
+//import components
+import Signup from '../Components/Signup';
+import Login from '../Components/Login';
+
 function landingPage() {
   return (
     <div>
-      landingPage will handle login & signup
+      <Login/>
+      <Signup/>
     </div>
   )
 }
