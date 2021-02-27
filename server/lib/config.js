92b3f30f0ba7fd8ceb120d72804210443f9d24b6
Mike <mibberson@3merge.ca>
2021-02-26 20:38:39 -0500

    chore(wip): placeholder models index

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,5 +1,7 @@
 const Q3 = require('q3-api');
+
 require('dotenv').config();
+require('./models');
 
 module.exports = Q3.config({
   enableServerToServer: false,
--- server/lib/worker.js
+++ server/lib/worker.js
@@ -1,1 +1,4 @@
+// temporarily required until next version Q3
+require('q3-api/lib/config/mongoose');
+require('./models');
 require('q3-api/lib/startQueue')(__dirname);
