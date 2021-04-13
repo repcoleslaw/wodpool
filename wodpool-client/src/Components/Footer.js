ae27c0ac36d6963679a37a9db4c89fb667b0a2c0
arr_cole <andrew.rr.cole@gmail.com>
2021-04-13 12:20:17 -0400

    Updated Footer to Grid
    changed div construction

--- wodpool-client/src/Components/Footer.js
+++ wodpool-client/src/Components/Footer.js
@@ -3,9 +3,11 @@
 import React from "react";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 
-import './Footer.css';
-import ThreeLogo from '../assets/3mergeLogo.png';
+import "./Footer.css";
+import ThreeLogo from "../assets/3mergeLogo.png";
 
 function Copyright() {
   return (
@@ -23,40 +25,54 @@ function Copyright() {
 function Footer() {
   return (
     <div className="main-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h4>Navigate</h4>
-            <ul className="unstyled-list">
-              <li>Home</li>
-              <li>User</li>
-              <li>Store</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>Connect</h4>
-            <ul className="unstyled-list">
-              <li>fb</li>
-              <li>ig</li>
-              <li>tw</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h4>Resources</h4>
-            <ul className="unstyled-list">
-              <li>Terms of Service</li>
-              <li>Privacy Policy</li>
-              <li>Assumption of Risk Waiver</li>
-            </ul>
-          </div>
-        </div>
-        <hr style={{marginLeft:"5em", marginRight:"5em"}}/>
-      </div>
-      <div className="Footer-PoweredBy">
+      <Container>
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid item xs={12} sm={3}>
+            <div className="col">
+              <h4>Navigate</h4>
+              <ul className="unstyled-list">
+                <li>Home</li>
+                <li>User</li>
+                <li>Store</li>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <div className="col">
+              <h4>Connect</h4>
+              <ul className="unstyled-list">
+                <li>Instagram</li>
+                <li>Twitter</li>
+                <li>Facebook</li>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <div className="col">
+              <h4>Resources</h4>
+              <ul className="unstyled-list">
+                <li>Terms of Service</li>
+                <li>Privacy Policy</li>
+                <li>Assumption of Risk Waiver</li>
+              </ul>
+            </div>
+          </Grid>
 
-        {" Powered by "}
-        <Link to="https://www.3merge.ca/">      <img style={{ height:"1.5em", marginLeft:"1em" }} src={ThreeLogo} alt="3merge-logo" /></Link>
+        </Grid>
         
+        <hr style={{ marginLeft: "1em", marginRight: "1em" }} />
+      </Container>
+
+      <div className="Footer-PoweredBy">
+        {" Powered by "}
+        <Link to="https://www.3merge.ca/">
+          {" "}
+          <img
+            style={{ height: "1.5em", marginLeft: "1em" }}
+            src={ThreeLogo}
+            alt="3merge-logo"
+          />
+        </Link>
       </div>
       <Copyright />
     </div>
