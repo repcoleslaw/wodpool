6c537b126c2a31ea7f7a2d7f75f3c4ba23357fca
arr_cole <andrew.rr.cole@gmail.com>
2021-04-18 18:26:54 -0400

    failing at using muithemes

---
+++ wodpool-client/src/util/ThemeProvider.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#B00909",
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
