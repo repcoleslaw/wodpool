5d44b1cd740f4416e1e0ac00f411f0b2ffb7bd65
arr_cole <andrew.rr.cole@gmail.com>
2021-04-19 09:30:36 -0400

    Registration Formated
    Updated styling on registration

--- wodpool-client/src/util/AuthProvider.js
+++ wodpool-client/src/util/AuthProvider.js
@@ -1,12 +1,29 @@
-import React from 'react'
-// need to check user authentication on log in
+// This is used to determine if a user is authenticated and
+// if they are allowed to visit the page they navigated to.
 
-function AuthProvider() {
+// If they are: they proceed to the page
+// If not: they are redirected to the login page.
+import React from 'react'
+
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+  // Add your own authentication on the below line.
+  const isLoggedIn = Boolean(true)
+
   return (
-    <div>
-      
-    </div>
+    <Route
+      {...rest}
+      render={props =>
+        isLoggedIn ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        )
+      }
+    />
   )
 }
 
-export default AuthProvider
+export default PrivateRoute
