6f05ed111b5f269f6b468dc3ad922dd723ed3158
arr_cole <andrew.rr.cole@gmail.com>
2021-04-21 21:09:33 -0400

    AuthRoute Component Added

---
+++ wodpool-client/src/util/AuthRoute.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Redirect} from 'react-router-dom';
+
+const AuthRoute = ({ component: Component, authenticated, ...rest }) => {
+  return(
+    <Route {...rest} render={(props) => authenticated === true 
+      ? 
+      <Component {...props}/> 
+      : 
+      <Redirect to='/landing'/>} />
+  )
+}
+
+export default AuthRoute
