dcfc0c751fda928178bd3ff6248f0691a21fad6f
arr_cole <andrew.rr.cole@gmail.com>
2021-04-22 17:29:22 -0400

    footer update

--- wodpool-client/src/Components/Footer.js
+++ wodpool-client/src/Components/Footer.js
@@ -5,11 +5,30 @@ import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./Footer.css";
 import ThreeLogo from "../assets/3mergeLogo.png";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign: "center",
+    padding: "4em",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    color: "white",
+  },
+  link: {
+    color: "white",
+  }
+}));
+
 function Copyright() {
+
   return (
     <Typography variant="body2" style={{color:"white"}} align="center">
       {"Copyright © "}
@@ -23,27 +42,18 @@ function Copyright() {
 }
 
 function Footer() {
+  const classes = useStyles();
   return (
     <div className="main-footer">
       <Container>
         <Grid container direction="row" justify="center" alignItems="center">
           <Grid item xs={12} sm={3}>
             <div className="col">
-              <h4>Navigate</h4>
-              <ul className="unstyled-list">
-                <li>Home</li>
-                <li>User</li>
-                <li>Store</li>
-              </ul>
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <div className="col">
               <h4>Connect</h4>
               <ul className="unstyled-list">
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>Facebook</li>
+                <li><Link className={classes.link} href="https://www.instagram.com/donttalkaboutwodpool/">Instagram</Link></li>
+                <li><Link className={classes.link} href="https://twitter.com/wodpool">Twitter</Link></li>
+                <li><Link className={classes.link} href="https://www.facebook.com/donttalkaboutwodpool">Facebook</Link></li>
               </ul>
             </div>
           </Grid>
