3bf80b56db6332e6fdd5c3c6d8e641c28c449727
arr_cole <andrew.rr.cole@gmail.com>
2021-08-20 16:13:36 -0400

    WIP  Alert Notification

--- wodpool-client/src/Components/AlertCard/AlertStyle.js
+++ wodpool-client/src/Components/AlertCard/AlertStyle.js
@@ -2,13 +2,12 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const alertStyle = makeStyles((theme) => ({
     container:{
-        flexGrow: 1,
         background:"rgba(255,0,0,0.5)",
         color:"#eee",
         borderRadius:"5px",
         border:"2px solid rgba(255,0,0,0.7)",
-        margin:".5em 0em"
-
+        margin:".5em",
+        padding:"0em 1em"
     }
 }));
 
