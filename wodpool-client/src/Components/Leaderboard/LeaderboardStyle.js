c19e46e2f6156ed7446f92ff8e0e317b3ad9f242
arr_cole <andrew.rr.cole@gmail.com>
2021-08-23 14:53:49 -0400

    WIP Leaderboard Fix

--- wodpool-client/src/Components/Leaderboard/LeaderboardStyle.js
+++ wodpool-client/src/Components/Leaderboard/LeaderboardStyle.js
@@ -1,9 +1,40 @@
 import {makeStyles} from '@material-ui/core/styles';
 
 const leaderboardStyles =  makeStyles((theme) => ({
-  accordionBase : {
-    backgroundColor:"rgba(0,0,0,.3)",
-  }
+  table : {
+    width:"100%"
+  },
+
+  th25:{
+    background:"rgba(0,0,0,0.8)",
+    width:"25%",
+
+  },
+  th50:{
+    background:"rgba(0,0,0,0.8)",
+    width:"50%",
+
+  },
+  thBtn:{
+    borderStyle:"none",
+    fontWeight:"bold",
+    background:"none",
+    color:"white",
+    width:"100%"
+  },
+  tRow:{
+    background:"rgba(0,0,0,0.3)",
+  },
+
+  tDataAlignCenter:{
+    padding:"0.5em",
+    textAlign:"center"
+  },
+  tDataAlignLeft:{
+    padding:"0.5em",
+    textAlign:"Left"
+  },
+
 }));
     
 
--- wodpool-client/src/Components/Leaderboard/PoolLeaderboard.js
+++ wodpool-client/src/Components/Leaderboard/PoolLeaderboard.js
@@ -1,95 +1,87 @@
 import React from 'react'
-import axios from 'axios'
-// use this page to sketch out the other shit that you get confused in.
 
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import usePools from '../usePools';
-
+//MUI
+import Paper from '@material-ui/core/Paper'
+import Button from "@material-ui/core/Button"
 
 //components
+import SortHelper from './SortHelper';
+//style
+import {useStyles} from '../../util/MakeStyles';
+import {leaderboardStyles} from './LeaderboardStyle';
 
 
-// style the table
-
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-      backgroundColor: "rgba(0,0,0)",
-      color: theme.palette.common.white,
-    },
-    body: {
-      fontSize: 14,
-    },
-  }))(TableCell);
-  
-  const StyledTableRow = withStyles((theme) => ({
-    root: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-      },
-    },
-  }))(TableRow);
-
-  const useStyles = makeStyles({
-    table: {
-      color:"black"
-    },
-    tableRow:{
-        color:"black"
-    }
-  });
-
-  // get table headers
-
-//QUESTION: Hw to hit Get and map into the rows.so we need to get all the rows related to ID + Week.
+  // Learning how to create sortable data
 
 
-
-// Converting Time back from flat Seconds
-// 2 Tables?
-//First Table is TOURNAMENT WIDE, where it descends based on points
-// Second Table is CURRENT WEEK, where it descends bassed on time. 
-
-// 1 Table for the page, drop down to toggle between modes.
-
-  
 function PoolLeaderboard(props) {
+    const classes = leaderboardStyles();
+    const global= useStyles();
+    const sh = SortHelper();
     const id = props.id
     const pool = props.pool
-    const classes = useStyles();
+    const competitors = pool.competitors;
 
+    const LeaderTable = (props) => {
+
+      const { items, requestSort, sortConfig } = sh.useSortableData(props.products);
+      const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+          return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+      };
+      return (
+        <table className={classes.table}>
+          <thead className={classes.thead}>
+            <tr>
+              <th className={classes.th25}>
+                <Button
+                  type="button"
+                  onClick={() => requestSort('pointsToDate')}
+                  className={classes.thBtn}
+                >
+                  Rank
+                </Button>
+              </th>
+              <th className={classes.th50}>
+                <Button
+                  type="button"
+                  onClick={() => requestSort('handle')}
+                  className={classes.thBtn}
+                  hover
+                >
+                  Handle
+                </Button>
+              </th>
+              <th  className={classes.th25}>
+                <Button
+                  type="button"
+                  onClick={() => requestSort('pointsToDate')}
+                  className={classes.thBtn}
+                >
+                  Points
+                </Button>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index} className={classes.tRow}>
+                <td className={classes.tDataAlignCenter}>{index + 1}</td>
+                <td className={classes.tDataAlignLeft}>{item.handle}</td>
+                <td className={classes.tDataAlignCenter}>{item.pointsToDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    };
 
     return (
-        <div className={classes.root}>
-            <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="customized table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="left">Rank</StyledTableCell>
-                            <StyledTableCell align="left">Handle </StyledTableCell>
-                            <StyledTableCell align="left">Score </StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {pool.competitors ? pool.competitors.map((competitor, index) => (
-                            <StyledTableRow  key={competitor.handle}>
-                                <StyledTableCell className={classes.tableRow} component="th" scope="row">{index + 1}</StyledTableCell>
-                                <StyledTableCell className={classes.tableRow} component="th" scope="row"> {competitor.handle}</StyledTableCell>
-                                <StyledTableCell className={classes.tableRow} align="left">{competitor.pointsToDate}</StyledTableCell>
-
-                            </StyledTableRow>
-                        )): <p>empty</p>}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-
-        </div>
+        <Paper className={global.paper}>
+         <LeaderTable products={competitors}/>
+        </Paper>
     )
 }
 
