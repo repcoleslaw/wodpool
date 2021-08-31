e637aff013fe84e9f182a157780699daacc4e905
arr_cole <andrew.rr.cole@gmail.com>
2021-08-31 10:40:00 -0400

    Update Terminology.js

--- wodpool-client/src/Components/AdditionalResources/Terminology.js
+++ wodpool-client/src/Components/AdditionalResources/Terminology.js
@@ -1,9 +1,17 @@
 import React from 'react'
+//MUI
+import Paper from '@material-ui/core/Paper'
+import { Typography } from '@material-ui/core';
+// Assets
+import {useStyles} from '../../util/MakeStyles'
 
 function Terminology() {
+  const global = useStyles();
   return (
-    <div>
-      This is the Terminology Card
+    <div className={global.section}>
+      <Paper className={global.paper}>
+      
+      </Paper>
     </div>
   )
 }
