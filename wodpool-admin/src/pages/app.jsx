d85b4db90ee4a1e62f628b79430fe6d63fca6ec3
Mike <mibberson@3merge.ca>
2021-12-08 12:54:10 -0500

    chore: cleanup repo

--- wodpool-admin/src/pages/app.jsx
+++ wodpool-admin/src/pages/app.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { get } from 'lodash';
 import { AdminRouter } from 'gatsby-theme-q3/src/components';
+import EmailEditor from 'q3-ui-emaileditor';
+import QueueLogs from 'q3-ui-queuelogs';
 import pages from '../components';
 
-export default () => (
+const App = () => (
   <AdminRouter
     AdminProps={{
       AppProps: {
+        addons: [EmailEditor, QueueLogs],
         pages,
       },
       ProfileActionsProps: {
         includeDocumentation: false,
       },
     }}
-    Gatekeeper={{
+    GatekeepProps={{
       redirectCheck: (e) =>
         get(e, 'role') !== 'Administrator'
           ? '/login'
@@ -21,3 +24,5 @@ export default () => (
     }}
   />
 );
+
+export default App;
